a0c7f136499c3507657a84a42fc468de76bf8af7
Eduardo Soares <eduschadesoares@users.noreply.github.com>
2018-11-21 00:08:40 -0200

    Serial

--- 4º Bimestre/IO - Serial/serial.c
+++ 4º Bimestre/IO - Serial/serial.c
@@ -116,7 +116,6 @@ int main(int argc, char **argv) {
 
     // Custom Stuff
 
-    
       // Verify first input
       while(read(fd, &letra, 1) >= 0) {      /* Get a char from serial port "fd"*/
         if(status == AVAILABLE) { //change to 2
